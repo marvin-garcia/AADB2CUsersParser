87c07f501da4f2da6844bb486aabdc7177dab7ed
Marvin Garcia <magar@microsoft.com>
2020-02-17 12:04:41 -0600

    uploaded .NET Framework solution to parse users

---
+++ B2CUsersParser/B2CUserParser/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace B2CUserParser
+{
+    class Program
+    {
+        private static string _sourceFile = ConfigurationManager.AppSettings["SourceFile"];
+        private static string _destinationFolder = ConfigurationManager.AppSettings["DestinationFolder"];
+
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(_destinationFolder))
+                Directory.CreateDirectory(_destinationFolder);
+
+            string fileContent = File.ReadAllText(_sourceFile);
+            JArray usersArray = JsonConvert.DeserializeObject<JArray>(fileContent);
+
+            Console.WriteLine($"Total users: {usersArray.Count()}");
+
+            int count = 1;
+            foreach (JObject user in usersArray)
+            {
+                string userString = JsonConvert.SerializeObject(user);
+                var signInName = user["signInNames"][0]["value"];
+
+                Console.WriteLine($"User {count}/{usersArray.Count()}: {signInName}");
+                File.WriteAllText($"{_destinationFolder}\\{signInName}.json", userString);
+                count++;
+            }
+
+            Console.WriteLine("Task completed. Press Enter to exit");
+            Console.Read();
+        }
+    }
+}
